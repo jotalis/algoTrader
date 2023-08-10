5650866d89bbd6c2d1bf499a357aa5666597fc52
jotalis <jayqwu@gmail.com>
2023-08-09 19:30:31 -0700

    checkpoint

--- dash_main.py
+++ dash_main.py
@@ -15,13 +15,15 @@ algo_df = mrr.get_mrr(df, levelsPeriod=21, levelsUpPercent=89, levelsDownPercent
 
 # Default Graphs
 candlestick_graph = go.Figure(go.Candlestick(
-    x=df['date'],
-    open=df['open'],
-    high=df['high'],
-    low=df['low'],
-    close=df['close']
+    x=candlestick_df['date'],
+    open=candlestick_df['open'],
+    high=candlestick_df['high'],
+    low=candlestick_df['low'],
+    close=candlestick_df['close']
 ))
+candlestick_graph.update_layout(xaxis_rangeslider_visible=False)
 algo_graph = go.Figure(data = [go.Scatter(x=algo_df[x]['date'],y=algo_df[x]['close'], name = x, showlegend = False) for x in algo_df]) 
+algo_graph.update_layout(xaxis_rangeslider_visible=False)
 
 # Initialize Dash App
 app = Dash(__name__)
@@ -36,79 +38,63 @@ app.layout = html.Div([
         id="candlestick_graph",
         figure=candlestick_graph,
         config={'displayModeBar': False},
-        style={'height': '750px'}  # Set the height for algo_graph
+        style={'height': '750px'},
+
+ 
     ),
     dcc.Graph(
         id="algo_graph",
         figure=algo_graph,
         config={'displayModeBar': False},
-        style={'height': '750px'}  # Set the height for algo_graph
+        style={'height': '750px'} ,
     ),
-    # html.Div([
-    #     dcc.Graph(id="candlestick_graph"),
-    #     dcc.Graph(id="algo_graph"), 
-    # ] , style={'display': 'flex', 'flexDirection': 'column'}),
+    dcc.Interval(
+        id = 'graph-update',
+        interval = 5000,
+        n_intervals = 0
+    ),
 ])
-
 
 # Define app callbacks
 @app.callback(
     Output('candlestick_graph', 'figure'),
     Output('algo_graph', 'figure'), 
     [Input(component_id='dropdown', component_property= 'value'),
-     Input('algo_graph', 'relayoutData')],
-     [State('candlestick_graph', 'figure'),
-      State('algo_graph', 'figure')]
+     Input('graph-update', 'n_intervals')],
+
 )
 # Update Function
-def display_graphs(value, relayoutData, candlestick_graph, algo_graph):
-    global df ,candlestick_df, algo_df, contract
+def display_graphs(value, intervals):
+    global df, candlestick_df, algo_df, contract
+    print(intervals)
+    df = pd.read_csv('data/' + value + '.csv')
 
-    if relayoutData == None or value != contract: # Contract Changed
-        df = pd.read_csv('data/' + value + '.csv')
-        algo_df = mrr.get_mrr(df, levelsPeriod=21, levelsUpPercent=89, levelsDownPercent=10)
-        
-        # Update candlestick graph
-        candlestick_graph = go.Figure(go.Candlestick(
-            x=df['date'],
-            open=df['open'],
-            high=df['high'],
-            low=df['low'],
-            close=df['close']
-        ))
-        candlestick_graph.update_layout(xaxis_rangeslider_visible=False)
-        
-        # Update algo graph
-        algo_graph = go.Figure(data = [go.Scatter(x=algo_df[x]['date'],y=algo_df[x]['close'], name = x, showlegend = False) for x in algo_df]) 
-        algo_graph.update_layout(xaxis_rangeslider_visible=True)
+    candlestick_df = df
+    algo_df = mrr.get_mrr(df, levelsPeriod=21, levelsUpPercent=89, levelsDownPercent=10)
+    
+    # Update candlestick graph
+    candlestick_graph = go.Figure(go.Candlestick(
+        x=candlestick_df['date'],
+        open=candlestick_df['open'],
+        high=candlestick_df['high'],
+        low=candlestick_df['low'],
+        close=candlestick_df['close']
+    ))
+    candlestick_graph.update_layout(xaxis_rangeslider_visible=False)
+    candlestick_graph.update_layout(transition_duration=500)
+    candlestick_graph.update_xaxes(type = 'category')
+    # Update algo graph
+    algo_graph = go.Figure(data = [go.Scatter(x=algo_df[x]['date'],y=algo_df[x]['close'], name = x, showlegend = False) for x in algo_df]) 
+    algo_graph.update_layout(xaxis_rangeslider_visible=False)
 
-        # Update current contract
+    if value != contract: # Contract Changed
+        print("contract changed")
         contract = value
-         
-        return [candlestick_graph, algo_graph] 
-
-    else: # Relay event callback
-
-        # Get min max of changed graph
-        xmin, xmax = algo_graph['layout']['xaxis']['range']
-        candlestick_xbound = candlestick_df[candlestick_df['date'].between(xmin, xmax)]
-
-        # Update candlestick graph
-        candlestick_graph = go.Figure(go.Candlestick(
-            x=candlestick_xbound['date'],
-            open=candlestick_xbound['open'],
-            high=candlestick_xbound['high'],
-            low=candlestick_xbound['low'],
-            close=candlestick_xbound['close']
-        ))
-        candlestick_graph.update_layout(xaxis_rangeslider_visible=False)
-
-        # Update algo graph
-        # Set min max of algo graph
-        # TODO: fix algo_graph ymin and ymax
-        algo_graph['layout']['yaxis']['range'] = [df['close'].min()-10, df['close'].max()+10]
-        algo_graph['layout']['yaxis']['autorange'] = False
+        with open("contract_request.txt", "w") as file:
+            file.write(value)
+            file.close()
 
     return [candlestick_graph, algo_graph]
 
 app.run(debug=True)
+
--- ib_main.py
+++ ib_main.py
@@ -1,27 +1,61 @@
 from ib_insync import *
 from algo_trader import constants
 import pandas as pd
+import os
 
-requested_contract = 'ES'
+
+# Callback function
+def on_new_bar(bars, hasNewBar):
+    print("is this begin called")
+    if hasNewBar:
+        util.df(bars).tail(1).to_csv('data/' + requested_contract + '.csv',
+                                    mode = 'a', index=False, header = False)
+
+# Default Contract
+requested_contract = 'MES'
+
 ib = IB()
 ib.connect('127.0.0.1', 7497, 0)
 
-#RequMest Market Data
-ib.reqMarketDataType(1)
+# Give time to connect to TWS
+while not ib.isConnected():
+    ib.sleep(0.01)
+print("Connected to TWS")
 
-#CANDLEBARS SHOW BID PRICE
-#useRTH = use Real Time Trading Hours
+# Request Real Time Market Data
+ib.reqMarketDataType(3)
+
+# Request candlebar data
+# useRTH = use Real Time Trading Hours
 bars = ib.reqHistoricalData(contract = constants.CONTRACTS[requested_contract],
     endDateTime = '',durationStr = '1 D', barSizeSetting='5 mins',
-    whatToShow = 'BID', useRTH = 0, formatDate = 1, 
+    whatToShow = 'BID', useRTH = 1, formatDate = 1, 
     keepUpToDate = True)
 
-#Converting to pandas dataframe
-df = util.df(bars)
+# Save to corresponding csv file
+df = util.df(bars).to_csv('data/' + requested_contract + '.csv', index=False)
 
-df = df.to_csv('data/' + requested_contract + '.csv', index=False)
+# Update calls callback function
+bars.updateEvent+= on_new_bar
 
+while True:
 
+    # Updates IB-Insync loop
+    ib.sleep(0.03)
 
+    # Check for new contract request
+    if os.path.exists("contract_request.txt"):
+        with open("contract_request.txt", "r") as file:
+            requested_contract = file.read()
+            print(requested_contract)
+            file.close()
+        os.remove("contract_request.txt")
 
+    if requested_contract != bars.contract.symbol:
+
+        bars = ib.reqHistoricalData(contract = constants.CONTRACTS[requested_contract],
+            endDateTime = '',durationStr = '2 D', barSizeSetting='15 mins',
+            whatToShow = 'BID', useRTH = 1, formatDate = 1, 
+            keepUpToDate = True)
+        df = util.df(bars).to_csv('data/' + requested_contract + '.csv', index=False)
 
